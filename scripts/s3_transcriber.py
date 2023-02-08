408231a40f71ae3c590e3a3d8fc228a5a4328864
Sergio Lucero <sergio.lucerovera@sunrun.com>
2023-02-08 01:03:00 -0300

    fixed several transcription details. Still cant handle m4a or aac

--- scripts/s3_transcriber.py
+++ scripts/s3_transcriber.py
@@ -11,9 +11,10 @@ transcribe = boto3.client('transcribe')
 
 bucket_name = 'quantcldata'
 folder_name = 'TITOVOX/CHOPPED'
+output_folder = 'titovox-transcripts'
 
 objects = s3.list_objects(Bucket=bucket_name, Prefix=folder_name)
-print('nOBJECTS:', len(objects['Contents']) # should be 39
+print('nOBJECTS:', len(objects['Contents'])) # should be 39
 
 # Loop through the objects in the S3 folder
 
@@ -22,18 +23,19 @@ for s3_object in objects['Contents']:   # could be done async
     file_name = s3_object['Key']
     media_format = file_name[-3:]
 
-    try:
+    if media_format in ['mp3','mp4']:
         transcribe_response = transcribe.start_transcription_job(
-            TranscriptionJobName=file_name,
-            LanguageCode='es-MX',   # perhaps es-ES?
+            TranscriptionJobName=(file_name.split('/')[-1])+'_v2',
+            LanguageCode='es-ES',
             MediaFormat=media_format,
             Media={
                 'MediaFileUri': f's3://{bucket_name}/{file_name}'
-            }
+            },
+            OutputBucketName=output_folder
         )
         head = f'Started transcription job for {file_name}'
         tail = transcribe_response["TranscriptionJob"]["TranscriptionJobName"]
         print(f'{head} with job name {tail}')
-    except:
+    else:
         print('COULD NOT HANDLE:', file_name)
 
