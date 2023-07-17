56eab2b675a207a39d5fe70ed645e0b075471986
Sergio Lucero <sergiolucero@gmail.com>
2023-07-17 16:51:55 -0700

    Update script_runner.py

--- ML/script_runner.py
+++ ML/script_runner.py
@@ -1,34 +1,35 @@
-
+import matplotlib.pyplot as plt
 import pandas as pd
+
 from tpot import TPOTClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
-#from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import roc_auc_score, roc_curve
-
-RANDOM = 42
-X = pd.read_csv('https://raw.githubusercontent.com/QuantaVox/titovox/main/ML/ParkSet2_X.csv')
-y = pd.read_csv('https://raw.githubusercontent.com/QuantaVox/titovox/main/ML/ParkSet2_y.csv')
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM)
-
-print('FITTING RandomForest')
 from sklearn.ensemble import RandomForestClassifier
 
-tpot = RandomForestClassifier(random_state=RANDOM)
-tpot.fit(X_train, y_train)
+def runit(RANDOM = 42):
+    X = pd.read_csv('https://raw.githubusercontent.com/QuantaVox/titovox/main/ML/ParkSet2_X.csv')
+    y = pd.read_csv('https://raw.githubusercontent.com/QuantaVox/titovox/main/ML/ParkSet2_y.csv')
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM)
 
-import matplotlib.pyplot as plt
-y_pred_prob = tpot.predict_proba(X_test)[:, 1]
-auc_score = roc_auc_score(y_test, y_pred_prob)
-print("AUC Score:", auc_score)
+    print('FITTING RandomForest')
 
-# Plot the ROC curve
-fpr, tpr, _ = roc_curve(y_test, y_pred_prob)
-plt.plot(fpr, tpr, color='blue', lw=2, label='ROC Curve')
-plt.plot([0, 1], [0, 1], color='gray', linestyle='--')
-plt.xlabel('False Positive Rate (FPR)')
-plt.ylabel('True Positive Rate (TPR)')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend()
-plt.show()
+    tpotRFC = RandomForestClassifier(random_state=RANDOM)
+    tpotRFC.fit(X_train, y_train)
+    return tpotRFC
+
+def auc_plot(tpot, X_test, y_test):
+    y_pred_prob = tpot.predict_proba(X_test)[:, 1]
+    auc_score = roc_auc_score(y_test, y_pred_prob)
+    print("AUC Score:", auc_score)
+    
+    # Plot the ROC curve
+    fpr, tpr, _ = roc_curve(y_test, y_pred_prob)
+    plt.plot(fpr, tpr, color='blue', lw=2, label='ROC Curve')
+    plt.plot([0, 1], [0, 1], color='gray', linestyle='--')
+    plt.xlabel('False Positive Rate (FPR)')
+    plt.ylabel('True Positive Rate (TPR)')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend()
+    plt.show()
